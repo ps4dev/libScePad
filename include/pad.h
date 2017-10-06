76b571c896ca0efac91c1db76f92e433c594f382
hitodama <hitodama@users.noreply.github.com>
2017-10-06 11:49:12 +0200

    Add initial reversing

--- include/pad.h
+++ include/pad.h
@@ -3,20 +3,26 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+int scePadClose(int handle);
+int scePadInit(void);
+int scePadOpen(int userID, int, int, void *);
+int scePadRead(int handle, void *data, int count);
+int scePadReadState(int handle, void *data);
+
 /* unreversed */
 
-int64_t scePadClose();
+// int64_t scePadClose();
 int64_t scePadDeviceOpen();
 int64_t scePadDisableVibration();
 int64_t scePadEnableUsbConnection();
 int64_t scePadGetControllerInformation();
 int64_t scePadGetDataInternal();
 int64_t scePadGetDeviceInfo();
-int64_t scePadInit();
+// int64_t scePadInit();
 int64_t scePadMbusInit();
-int64_t scePadOpen();
-int64_t scePadRead();
-int64_t scePadReadState();
+// int64_t scePadOpen();
+// int64_t scePadRead();
+// int64_t scePadReadState();
 int64_t scePadResetLightBar();
 int64_t scePadResetOrientation();
 int64_t scePadSetAngularVelocityDeadbandState();
